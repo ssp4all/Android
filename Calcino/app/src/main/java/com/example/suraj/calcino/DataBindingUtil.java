735139394a59c173e0abb7e5c445c99b72752ae6
ssp <2015suraj.pawar@ves.ac.in>
2018-03-21 10:16:54 +0530

    add Calculator

---
+++ Calcino/app/src/main/java/com/example/suraj/calcino/DataBindingUtil.java
@@ -0,0 +1,8 @@
+package com.example.suraj.calcino;
+
+/**
+ * Created by suraj on 02-08-2017.
+ */
+
+class DataBindingUtil {
+}
---
+++ Calcino/app/src/test/java/com/example/suraj/calcino/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.suraj.calcino;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by suraj on 02-08-2017.
+ */
+public class MainActivityTest
+
+{
+
+}
